9621caadffde0a822ba5d624ed24668084862e5e
Simats <marc.simats@gmail.com>
2017-01-23 20:01:48 +0100

    Potenciometre

---
+++ Potenciometre/Potenciometre.ino
@@ -0,0 +1,36 @@
+/****************************************************************
+**                                                             **
+**                            TÍTOL:                           **
+**                        Potenciometre                        **
+**                                                             **
+**   NOM: Marc Simats                         Data:23/01/17    **
+****************************************************************/
+//************************** INCLUDE ****************************
+
+
+//************************* VARIABLES ***************************
+
+
+//*************************** SETUP *****************************
+
+void setup() {
+  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  int potPin = 0;     // Pin d'entrada per al potenciometre 
+  int ledPin = 13;    // Pin de sortida per al LED
+  pinMode(ledPin, OUTPUT);   // declara el pin del LED com a sortida
+}
+
+
+//*************************** LOOP ******************************
+
+void loop() {
+   digitalWrite(ledPin, HIGH);  // engega el LED 
+   delay(analogRead(potPin));   // Llegeix el valor del potenciometre
+   digitalWrite(ledPin, LOW);   // Apaga el LED 
+   delay(analogRead(potPin));
+
+}
+
+
+//************************ FUNCIONS *****************************
